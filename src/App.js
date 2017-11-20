d4c1a319768a9ea39708e74f216491e5acd132e3
Nick Yahnke <nthitz@gmail.com>
2017-11-20 11:10:47 -0800

    graph

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import moment from 'moment'
+import {nest} from 'd3-collection'
+import Stack from './Stack'
+import players from './players.json'
+import allGames from './allGames.json'
 
+allGames.sort((a, b) => {
+  return a.time - b.time
+})
+
+allGames.forEach(game => {
+  game.time = moment(game.time)
+})
+
+const byGame = nest()
+  .key(d => d.game)
+  .entries(allGames)
+
+console.log(byGame)
+
+byGame.forEach((machine) => {
+  const games = machine.values
+  let count = 0
+  const cumulativeGames = games.map(game => {
+    count ++
+    game.count = count
+    return game
+  })
+  machine.cumulativeGames = cumulativeGames
+})
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Stack data={byGame} allGames={allGames} />
       </div>
     );
   }
